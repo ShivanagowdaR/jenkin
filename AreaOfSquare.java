b9406cf954124acac78a63a56b39e93a6e175d47
ShivanagowdaR <rshivanagowda22@gmail.com>
2026-01-29 22:05:37 +0530

    Update AreaOfSquare.java

--- AreaOfSquare.java
+++ AreaOfSquare.java
@@ -3,7 +3,7 @@ class AreaOfSquare {
     public static void main(String[] args) {
 
         System.out.print("Enter side of square: ");
-        int side = 2;
+        int side = 4;
 
         int area = side * side;
         System.out.println("Area of square = " + area);
